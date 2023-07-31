e70646aae636402f0a1374661b7cd90e01f128e0
Jersh <joshajacobsen@gmail.com>
2023-07-30 19:55:24 -0500

    Initial progress commit. Have a basic tab navigator with a calendar and split empty screens.

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+const config = {
+    verbose: true,
+    clearMocks: true,
+    transformIgnorePatterns: [
+        "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)"
+    ]
+}
+
+module.exports = {...config, preset: "jest-expo"};
